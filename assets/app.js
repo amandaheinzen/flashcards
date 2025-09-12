4c476cef405f1f87b75749b961f9c5c1a9b500c4
amandaheinzen <amanda.heinzen@escola.pr.gov.br>
2025-09-12 10:22:43 -0300

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+function criaCartao(categoria, pergunta, resposta) {
+let container = document.getElementById('container')
+let cartao = document.createElement('article')
+cartao.className = 'cartao'
+
+cartao.innerHTML =
+<div class="cartao__conteudo">
+                <h3>Xadrez</h3>
+                <div class="cartao__conteudo__pergunta">
+                    <p>Qual é o melhor jogador de xadrez do mundo?</p> 
+                </div>
+                <div class="cartao__conteudo__resposta">
+                    <p>Magnus Carlsen</p>
+                </div>
+            </div>
+
+            container.appendChild(cartao)
+}
---
+++ assets/perguntas.js
@@ -0,0 +1,55 @@
+criaCartao(
+'Xadrez',
+'Qual é o melhor jogador de xadrez do mundo?',
+'Magnus Carlsen'
+
+)
+
+criaCartao(
+'Xadrez',
+'Quem foi o primeiro brasileiro a vencer o Magnus Carlsen?',
+'Glaucio Cella, em uma simultânea em 2014.'
+)
+
+criaCartao(
+'Xadrez',
+'Quem foi o jogador que foi campeão mundial por mais tempo?',
+'Emanuel Lasker, por 27 anos, de 1894-1921.'
+)
+
+criaCartao(
+'Xadrez',
+'Qual foi o maior rating fide da história do xadrez?',
+'2882, atingido por Magnus Carlsen em 2014.'
+)
+
+criaCartao(
+'Xadrez',
+'Quem é o atual campeão mundial?',
+'Dommaraju Gukesh'
+)
+
+criaCartao(
+'Xadrez',
+'Qual foi o jogador mais jovem a se tornar Grande Mestre?',
+'Abhimanyu Mishra, com 12 anos, em 2021.'
+)
+
+criaCartao(
+'Xadrez',
+'Qual foi o primeiro jogador a se tornar Grande Mestre?',
+'Jose Raúl Capablanca'
+)
+
+criaCartao(
+'Xadrez',
+'Qual é a plataforma mais utilizada para se jogar xadrez?',
+'Chess.com'
+)
+
+criaCartao(
+'Xadrez',
+'Quem é o jogador com mais campeonatos mundiais do mundo?',
+'Garry Kasparov, com 9 títulos.'
+)
+
