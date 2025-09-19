f5e7f8dbfd2776118731ce0091f51dd484a5b1b4
amandaheinzen <amanda.heinzen@escola.pr.gov.br>
2025-09-19 09:17:49 -0300

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -3,16 +3,28 @@ let container = document.getElementById('container')
 let cartao = document.createElement('article')
 cartao.className = 'cartao'
 
-cartao.innerHTML =
+cartao.innerHTML = `
 <div class="cartao__conteudo">
-                <h3>Xadrez</h3>
+                <h3>${categoria}</h3>
                 <div class="cartao__conteudo__pergunta">
-                    <p>Qual é o melhor jogador de xadrez do mundo?</p> 
+                    <p>${pergunta}</p> 
                 </div>
                 <div class="cartao__conteudo__resposta">
-                    <p>Magnus Carlsen</p>
+                    <p>${resposta}</p>
                 </div>
             </div>
+`
+
+let respostaEstaVisivel = false
+
+
+function viraCartao() {
+respostaEstaVisivel = !respostaEstaVisivel
+cartao.classList.toggle('active',respostaEstaVisivel)
+
+}
+
+cartao.addEventListener('click', viraCartao)
 
             container.appendChild(cartao)
 }
